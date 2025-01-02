1ebf1d60445b1ee82adf0a87307f0771a06f91a2
gaji12345 <gajimax12345@gmail.com>
2025-01-02 16:55:56 +0530

    enums done

---
+++ src/main/java/com/example/InventoryManagmentSystem/enums/TransactionStatus.java
@@ -0,0 +1,8 @@
+package com.example.InventoryManagmentSystem.enums;/*  gaajiCode
+    99
+    02/01/2025
+    */
+
+public enum TransactionStatus {
+    PENDING,PROCESSING,COMPLETED,CANCELLED
+}
---
+++ src/main/java/com/example/InventoryManagmentSystem/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.example.InventoryManagmentSystem.enums;/*  gaajiCode
+    99
+    02/01/2025
+    */
+
+public enum UserRole {
+    ADMIN,MANAGER
+}
